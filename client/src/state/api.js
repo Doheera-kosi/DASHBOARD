f116cf1fc7b1b19584f9651d0a23d330436fab98
Doheera-kosi <kupourevans99@gmail.com>
2024-02-27 20:35:24 +0000

    - Made changes to server-side controllers/sales.js, index.js, models/ProductStat.js, and routes/sales.js to enhance performance and features.

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
   reducerPath: "adminApi",
-  tagTypes: ["User", "Products", "Customers", "Geography"],
+  tagTypes: ["User", "Products", "Customers", "Geography", "Sales"],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
@@ -32,6 +32,11 @@ export const api = createApi({
     getGeography: build.query({
       query: () => "client/geography",
       providesTags: ["Geography"]
+    }),
+
+    getSales: build.query({
+      query: () => "sales/sales",
+      providesTags: ["Sales"]
     })
   }),
 });
@@ -42,4 +47,5 @@ export const {
   useGetCustomersQuery,
   useGetTransactionsQuery,
   useGetGeographyQuery,
+  useGetSalesQuery,
 } = api;
